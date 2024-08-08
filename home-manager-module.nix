72d3f7b2c5e622bc4ce92e86ba72a689e40e278e
Collin Diekvoss <Collin@Diekvoss.com>
2024-08-08 12:38:48 -0500

    warn -> debug clean profile not found
    Fix url based flakes
    
    crate2nix
    
    fix linux build need to revisit crate2nix
    
    module
    
    darwin-module.nix
    
    crate2nix works
    
    re add modules
    
    cleanup
    
    maybe fixed

--- home-manager-module.nix
+++ home-manager-module.nix
@@ -1,16 +1,29 @@
 # Notice: this file will only exist until this pr is merged https://github.com/nix-community/home-manager/pull/5304
 self: { config, lib, pkgs, ... }:
 
-let cfg = config.programs.nh;
-in {
+let
+  cfg = config.programs.nh;
+  nh_darwin = self.packages.${pkgs.stdenv.hostPlatform.system}.nh_darwin;
+  nh = (pkgs.runCommand "${nh_darwin.pname}-docker-compat-${nh_darwin.version}"
+    {
+      outputs = [ "out" ];
+      inherit (nh_darwin) meta;
+    } ''
+    mkdir -p $out/bin
+    ln -s ${nh_darwin}/bin/nh_darwin $out/bin/nh
+  '');
+in
+{
   meta.maintainers = with lib.maintainers; [ johnrtitor ];
 
   options.programs.nh = {
     enable = lib.mkEnableOption "nh_darwin, yet another Nix CLI helper. Works on NixOS, NixDarwin, and HomeManager Standalone";
 
     package = lib.mkPackageOption pkgs "nh" { } // {
-      default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      default = nh_darwin;
     };
+
+    alias = lib.mkEnableOption "Enable alias of nh_darwin to nh";
 
     flake = lib.mkOption {
       type = lib.types.nullOr lib.types.path;
@@ -30,7 +43,7 @@ in {
     }];
 
     home = lib.mkIf cfg.enable {
-      packages = [ cfg.package ];
+      packages = [ cfg.package ] ++ lib.optionals (cfg.alias) [ nh ];
       sessionVariables = lib.mkIf (cfg.flake != null) { FLAKE = cfg.flake; };
     };
   };
