55b8001f678e1ed1b555bcb9a6292459a86c8430
Collin Diekvoss <Collin@Diekvoss.com>
2024-08-07 23:27:00 -0500

    sudo args

--- src/nixos.rs
+++ src/nixos.rs
@@ -116,8 +116,8 @@ impl OsSubcommandArgs {
                         .message("Checking out flake.lock")
                         .build()?
                         .exec()?;
-                } else {
-                    panic!("Conflicts dectected that were more than just flake.lock");
+                } else if conflict != "" {
+                    panic!("Conflicts dectected that were more than just flake.lock, {conflict:?}");
                 }
             }
 
