ToyVo/nh_darwin

Rename to nh_darwin
rebase

Rename to nh-darwin, fix devshell not working on darwin, add modules for home manager and modules that can be used post modules being builtin to nix-darwin and home-manager

complete rename

move repo, fix uid on macos

flake.nix
@@ -3,39 +3,48 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        # experimental
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ] (system: function nixpkgs.legacyPackages.${system});
+  outputs =
+    { self
+    , nixpkgs
+    ,
+    }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          # experimental
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ]
+          (system: function nixpkgs.legacyPackages.${system});
 
-    rev = self.shortRev or self.dirtyShortRev or "dirty";
-  in {
-    overlays.default = final: prev: {
-      nh = final.callPackage ./package.nix {
-        inherit rev;
+      rev = self.shortRev or self.dirtyShortRev or "dirty";
+    in
+    {
+      overlays.default = final: prev: {
+        nh_darwin = self.packages.${final.stdenv.system}.nh_darwin;
       };
+
+      packages = forAllSystems (pkgs: rec {
+        nh_darwin = pkgs.callPackage ./package.nix {
+          inherit rev;
+        };
+        default = nh_darwin;
+      });
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./devshell.nix { };
+      });
+
+      nixosModules.default = import ./module.nix self;
+      # use this module before this pr is merged https://github.com/LnL7/nix-darwin/pull/942
+      nixDarwinModules.prebuiltin = import ./darwin-module.nix self;
+      # use this module after that pr is merged
+      nixDarwinModules.default = import ./module.nix self;
+      # use this module before this pr is merged https://github.com/nix-community/home-manager/pull/5304
+      homeManagerModules.prebuiltin = import ./home-manager-module.nix self;
+      # use this module after that pr is merged
+      homeManagerModules.default = import ./module.nix self;
     };
-
-    packages = forAllSystems (pkgs: rec {
-      nh = pkgs.callPackage ./package.nix {
-        inherit rev;
-      };
-      default = nh;
-    });
-
-    devShells = forAllSystems (pkgs: {
-      default = pkgs.callPackage ./devshell.nix {};
-    });
-
-    nixosModules.default = import ./module.nix self;
-    nixDarwinModules.default = import ./darwin-module.nix self;
-  };
 }

home-manager-module.nix
@@ -1,0 +1,37 @@
+# Notice: this file will only exist until this pr is merged https://github.com/nix-community/home-manager/pull/5304
+self: { config, lib, pkgs, ... }:
+
+let cfg = config.programs.nh;
+in {
+  meta.maintainers = with lib.maintainers; [ johnrtitor ];
+
+  options.programs.nh = {
+    enable = lib.mkEnableOption "nh_darwin, yet another Nix CLI helper. Works on NixOS, NixDarwin, and HomeManager Standalone";
+
+    package = lib.mkPackageOption pkgs "nh" { } // {
+      default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    };
+
+    flake = lib.mkOption {
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+      description = ''
+        The path that will be used for the `FLAKE` environment variable.
+
+        `FLAKE` is used by nh_darwin as the default flake for performing actions, like `nh_darwin os switch`.
+      '';
+    };
+  };
+
+  config = {
+    assertions = [{
+      assertion = (cfg.flake != null) -> !(lib.hasSuffix ".nix" cfg.flake);
+      message = "nh.flake must be a directory, not a nix file";
+    }];
+
+    home = lib.mkIf cfg.enable {
+      packages = [ cfg.package ];
+      sessionVariables = lib.mkIf (cfg.flake != null) { FLAKE = cfg.flake; };
+    };
+  };
+}

package.nix
@@ -1,67 +1,67 @@
-{
-  stdenv,
-  lib,
-  rustPlatform,
-  installShellFiles,
-  makeBinaryWrapper,
-  darwin,
-  nvd,
-  use-nom ? true,
-  nix-output-monitor ? null,
-  rev ? "dirty",
+{ stdenv
+, lib
+, rustPlatform
+, installShellFiles
+, makeBinaryWrapper
+, darwin
+, nvd
+, use-nom ? true
+, nix-output-monitor ? null
+, rev ? "dirty"
+,
 }:
 assert use-nom -> nix-output-monitor != null; let
-  runtimeDeps = [nvd] ++ lib.optionals use-nom [nix-output-monitor];
+  runtimeDeps = [ nvd ] ++ lib.optionals use-nom [ nix-output-monitor ];
   cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 in
-  rustPlatform.buildRustPackage {
-    pname = "nh";
-    version = "${cargoToml.package.version}-${rev}";
+rustPlatform.buildRustPackage {
+  pname = "nh_darwin";
+  version = "${cargoToml.package.version}-${rev}";
 
-    src = lib.fileset.toSource {
-      root = ./.;
-      fileset =
-        lib.fileset.intersection
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset =
+      lib.fileset.intersection
         (lib.fileset.fromSource (lib.sources.cleanSource ./.))
         (lib.fileset.unions [
           ./src
           ./Cargo.toml
           ./Cargo.lock
         ]);
-    };
+  };
 
-    strictDeps = true;
+  strictDeps = true;
 
-    nativeBuildInputs = [
-      installShellFiles
-      makeBinaryWrapper
-    ];
+  nativeBuildInputs = [
+    installShellFiles
+    makeBinaryWrapper
+  ];
 
-    buildInputs = lib.optionals stdenv.isDarwin [darwin.apple_sdk.frameworks.SystemConfiguration];
+  buildInputs = lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.SystemConfiguration ];
 
-    doCheck = false; # faster builds
+  doCheck = false; # faster builds
 
-    preFixup = ''
-      mkdir completions
-      $out/bin/nh completions --shell bash > completions/nh.bash
-      $out/bin/nh completions --shell zsh > completions/nh.zsh
-      $out/bin/nh completions --shell fish > completions/nh.fish
+  preFixup = ''
+    mkdir completions
+    $out/bin/nh_darwin completions --shell bash > completions/nh_darwin.bash
+    $out/bin/nh_darwin completions --shell zsh > completions/nh_darwin.zsh
+    $out/bin/nh_darwin completions --shell fish > completions/nh_darwin.fish
 
-      installShellCompletion completions/*
-    '';
+    installShellCompletion completions/*
+  '';
 
-    postFixup = ''
-      wrapProgram $out/bin/nh \
-        --prefix PATH : ${lib.makeBinPath runtimeDeps}
-    '';
+  postFixup = ''
+    wrapProgram $out/bin/nh_darwin \
+      --prefix PATH : ${lib.makeBinPath runtimeDeps}
+  '';
 
-    cargoLock.lockFile = ./Cargo.lock;
+  cargoLock.lockFile = ./Cargo.lock;
 
-    meta = {
-      description = "Yet another nix cli helper";
-      homepage = "https://github.com/ToyVo/nh";
-      license = lib.licenses.eupl12;
-      mainProgram = "nh";
-      maintainers = with lib.maintainers; [drupol viperML ToyVo];
-    };
-  }
+  meta = {
+    description = "Yet another nix cli helper. Works on NixOS, NixDarwin, and HomeManager Standalone";
+    homepage = "https://github.com/ToyVo/nh_darwin";
+    license = lib.licenses.eupl12;
+    mainProgram = "nh_darwin";
+    maintainers = with lib.maintainers; [ drupol viperML ToyVo ];
+  };
+}

src/logging.rs
@@ -55,7 +55,7 @@ where
 pub(crate) fn setup_logging(verbose: bool) -> Result<()> {
     color_eyre::config::HookBuilder::default()
         .display_location_section(true)
-        .panic_section("Please report the bug at https://github.com/ToyVo/nh/issues")
+        .panic_section("Please report the bug at https://github.com/ToyVo/nh_darwin/issues")
         .display_env_section(false)
         .install()?;
 

ToyVo/nh_plus

flake.nix
@@ -3,39 +3,48 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        # experimental
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ] (system: function nixpkgs.legacyPackages.${system});
+  outputs =
+    { self
+    , nixpkgs
+    ,
+    }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          # experimental
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ]
+          (system: function nixpkgs.legacyPackages.${system});
 
-    rev = self.shortRev or self.dirtyShortRev or "dirty";
-  in {
-    overlays.default = final: prev: {
-      nh = final.callPackage ./package.nix {
-        inherit rev;
+      rev = self.shortRev or self.dirtyShortRev or "dirty";
+    in
+    {
+      overlays.default = final: prev: {
+        nh = self.packages.${final.stdenv.system}.nh;
       };
+
+      packages = forAllSystems (pkgs: rec {
+        nh = pkgs.callPackage ./package.nix {
+          inherit rev;
+        };
+        default = nh;
+      });
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.callPackage ./devshell.nix { };
+      });
+
+      nixosModules.default = import ./module.nix self;
+      # use this module before this pr is merged https://github.com/LnL7/nix-darwin/pull/942
+      nixDarwinModules.prebuiltin = import ./darwin-module.nix self;
+      # use this module after that pr is merged
+      nixDarwinModules.default = import ./module.nix self;
+      # use this module before this pr is merged https://github.com/nix-community/home-manager/pull/5304
+      homeManagerModules.prebuiltin = import ./home-manager-module.nix self;
+      # use this module after that pr is merged
+      homeManagerModules.default = import ./module.nix self;
     };
-
-    packages = forAllSystems (pkgs: rec {
-      nh = pkgs.callPackage ./package.nix {
-        inherit rev;
-      };
-      default = nh;
-    });
-
-    devShells = forAllSystems (pkgs: {
-      default = pkgs.callPackage ./devshell.nix {};
-    });
-
-    nixosModules.default = import ./module.nix self;
-    nixDarwinModules.default = import ./darwin-module.nix self;
-  };
 }

home-manager-module.nix
@@ -1,0 +1,37 @@
+# Notice: this file will only exist until this pr is merged https://github.com/nix-community/home-manager/pull/5304
+self: { config, lib, pkgs, ... }:
+
+let cfg = config.programs.nh;
+in {
+  meta.maintainers = with lib.maintainers; [ johnrtitor ];
+
+  options.programs.nh = {
+    enable = lib.mkEnableOption "nh, yet another Nix CLI helper";
+
+    package = lib.mkPackageOption pkgs "nh" { } // {
+      default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    };
+
+    flake = lib.mkOption {
+      type = lib.types.nullOr lib.types.path;
+      default = null;
+      description = ''
+        The path that will be used for the `FLAKE` environment variable.
+
+        `FLAKE` is used by nh as the default flake for performing actions, like `nh os switch`.
+      '';
+    };
+  };
+
+  config = {
+    assertions = [{
+      assertion = (cfg.flake != null) -> !(lib.hasSuffix ".nix" cfg.flake);
+      message = "nh.flake must be a directory, not a nix file";
+    }];
+
+    home = lib.mkIf cfg.enable {
+      packages = [ cfg.package ];
+      sessionVariables = lib.mkIf (cfg.flake != null) { FLAKE = cfg.flake; };
+    };
+  };
+}

package.nix
@@ -1,67 +1,67 @@
-{
-  stdenv,
-  lib,
-  rustPlatform,
-  installShellFiles,
-  makeBinaryWrapper,
-  darwin,
-  nvd,
-  use-nom ? true,
-  nix-output-monitor ? null,
-  rev ? "dirty",
+{ stdenv
+, lib
+, rustPlatform
+, installShellFiles
+, makeBinaryWrapper
+, darwin
+, nvd
+, use-nom ? true
+, nix-output-monitor ? null
+, rev ? "dirty"
+,
 }:
 assert use-nom -> nix-output-monitor != null; let
-  runtimeDeps = [nvd] ++ lib.optionals use-nom [nix-output-monitor];
+  runtimeDeps = [ nvd ] ++ lib.optionals use-nom [ nix-output-monitor ];
   cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 in
-  rustPlatform.buildRustPackage {
-    pname = "nh";
-    version = "${cargoToml.package.version}-${rev}";
+rustPlatform.buildRustPackage {
+  pname = "nh";
+  version = "${cargoToml.package.version}-${rev}";
 
-    src = lib.fileset.toSource {
-      root = ./.;
-      fileset =
-        lib.fileset.intersection
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset =
+      lib.fileset.intersection
         (lib.fileset.fromSource (lib.sources.cleanSource ./.))
         (lib.fileset.unions [
           ./src
           ./Cargo.toml
           ./Cargo.lock
         ]);
-    };
+  };
 
-    strictDeps = true;
+  strictDeps = true;
 
-    nativeBuildInputs = [
-      installShellFiles
-      makeBinaryWrapper
-    ];
+  nativeBuildInputs = [
+    installShellFiles
+    makeBinaryWrapper
+  ];
 
-    buildInputs = lib.optionals stdenv.isDarwin [darwin.apple_sdk.frameworks.SystemConfiguration];
+  buildInputs = lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.SystemConfiguration ];
 
-    doCheck = false; # faster builds
+  doCheck = false; # faster builds
 
-    preFixup = ''
-      mkdir completions
-      $out/bin/nh completions --shell bash > completions/nh.bash
-      $out/bin/nh completions --shell zsh > completions/nh.zsh
-      $out/bin/nh completions --shell fish > completions/nh.fish
+  preFixup = ''
+    mkdir completions
+    $out/bin/nh completions --shell bash > completions/nh.bash
+    $out/bin/nh completions --shell zsh > completions/nh.zsh
+    $out/bin/nh completions --shell fish > completions/nh.fish
 
-      installShellCompletion completions/*
-    '';
+    installShellCompletion completions/*
+  '';
 
-    postFixup = ''
-      wrapProgram $out/bin/nh \
-        --prefix PATH : ${lib.makeBinPath runtimeDeps}
-    '';
+  postFixup = ''
+    wrapProgram $out/bin/nh \
+      --prefix PATH : ${lib.makeBinPath runtimeDeps}
+  '';
 
-    cargoLock.lockFile = ./Cargo.lock;
+  cargoLock.lockFile = ./Cargo.lock;
 
-    meta = {
-      description = "Yet another nix cli helper";
-      homepage = "https://github.com/ToyVo/nh";
-      license = lib.licenses.eupl12;
-      mainProgram = "nh";
-      maintainers = with lib.maintainers; [drupol viperML ToyVo];
-    };
-  }
+  meta = {
+    description = "Fork of nh with added support for nix-darwin and other features.";
+    homepage = "https://github.com/ToyVo/nh_plus";
+    license = lib.licenses.eupl12;
+    mainProgram = "nh";
+    maintainers = with lib.maintainers; [ ToyVo ];
+  };
+}

src/logging.rs
@@ -55,7 +55,7 @@ where
 pub(crate) fn setup_logging(verbose: bool) -> Result<()> {
     color_eyre::config::HookBuilder::default()
         .display_location_section(true)
-        .panic_section("Please report the bug at https://github.com/ToyVo/nh/issues")
+        .panic_section("Please report the bug at https://github.com/ToyVo/nh_plus/issues")
         .display_env_section(false)
         .install()?;
 