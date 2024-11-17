ToyVo/nh_darwin

warn -> debug clean profile not found
Fix url based flakes

crate2nix

fix linux build need to revisit crate2nix

module

darwin-module.nix

crate2nix works

re add modules

cleanup

maybe fixed

Containerfile
@@ -1,0 +1,7 @@
+# Make sure I can test builds on linux from macos
+FROM nixos/nix
+RUN mkdir /src
+WORKDIR /src
+ADD . ./
+RUN nix --extra-experimental-features "nix-command flakes" build .#nh_darwin --accept-flake-config
+CMD ["./result/bin/nh_darwin"]

devshell.nix
@@ -1,33 +1,43 @@
-{ mkShell
-, rust-analyzer-unwrapped
-, rustfmt
-, clippy
-, nvd
-, nix-output-monitor
-, cargo
-, rustc
-, rustPlatform
-,
-}:
-mkShell {
-  strictDeps = true;
+{ inputs, ... }: {
+  perSystem = { pkgs, lib, ... }: {
+    devshells.default = {
+      imports = [
+        "${inputs.devshell}/extra/language/c.nix"
+        # "${devshell}/extra/language/rust.nix"
+      ];
 
-  nativeBuildInputs = [
-    cargo
-    rustc
+      commands = with pkgs; [
+        { package = cargo; category = "rust"; }
+      ];
 
-    rust-analyzer-unwrapped
-    rustfmt
-    clippy
-    nvd
-    nix-output-monitor
-  ];
+      packages = with pkgs; [
+        cargo
+        rustc
+        rust-analyzer-unwrapped
+        rustfmt
+        clippy
+        nvd
+        nix-output-monitor
+      ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.SystemConfiguration ];
 
-  buildInputs = [ ];
+      env = [
+        {
+          name = "NH_NOM";
+          value = "1";
+        }
+        {
+          name = "RUST_LOG";
+          value = "nh_darwin=trace";
+        }
+        {
+          name = "RUST_SRC_PATH";
+          value = "${pkgs.rustPlatform.rustLibSrc}";
+        }
+      ];
 
-  env = {
-    NH_NOM = "1";
-    RUST_LOG = "nh_darwin=trace";
-    RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+      language.c = {
+        libraries = lib.optional pkgs.stdenv.isDarwin pkgs.libiconv;
+      };
+    };
   };
 }

flake.nix
@@ -1,50 +1,68 @@
 {
+  description = "nh_darwin";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    crate2nix.url = "github:nix-community/crate2nix";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "eigenvalue.cachix.org-1:ykerQDDa55PGxU25CETy9wF6uVDpadGGXYrFNJA3TUs=";
+    extra-substituters = "https://eigenvalue.cachix.org";
+    allow-import-from-derivation = true;
   };
 
   outputs =
-    { self
+    inputs @ { self
     , nixpkgs
-    ,
-    }:
-    let
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-          "aarch64-linux"
-          # experimental
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ]
-          (system: function nixpkgs.legacyPackages.${system});
+    , flake-parts
+    , crate2nix
+    , devshell
+    }: flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-      rev = self.shortRev or self.dirtyShortRev or "dirty";
-    in
-    {
-      overlays.default = final: prev: {
-        nh_darwin = self.packages.${final.stdenv.system}.nh_darwin;
+      imports = [
+        flake-parts.flakeModules.easyOverlay
+        devshell.flakeModule
+        ./devshell.nix
+      ];
+
+      perSystem = { pkgs, config, ... }: {
+        overlayAttrs = {
+          inherit (config.packages) nh_darwin;
+        };
+        packages = rec {
+          nh_darwin = pkgs.callPackage ./package.nix {
+            inherit crate2nix;
+            rev = self.shortRev or self.dirtyShortRev or "dirty";
+          };
+          default = nh_darwin;
+        };
       };
 
-      packages = forAllSystems (pkgs: rec {
-        nh_darwin = pkgs.callPackage ./package.nix {
-          inherit rev;
-        };
-        default = nh_darwin;
-      });
-
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.callPackage ./devshell.nix { };
-      });
-
-      nixosModules.default = import ./module.nix self;
-      # use this module before this pr is merged https://github.com/LnL7/nix-darwin/pull/942
-      nixDarwinModules.prebuiltin = import ./darwin-module.nix self;
-      # use this module after that pr is merged
-      nixDarwinModules.default = import ./module.nix self;
-      # use this module before this pr is merged https://github.com/nix-community/home-manager/pull/5304
-      homeManagerModules.prebuiltin = import ./home-manager-module.nix self;
-      # use this module after that pr is merged
-      homeManagerModules.default = import ./module.nix self;
+      flake = {
+        nixosModules.default = import ./module.nix self;
+        # use this module before this pr is merged https://github.com/LnL7/nix-darwin/pull/942
+        nixDarwinModules.prebuiltin = import ./darwin-module.nix self;
+        # use this module after that pr is merged
+        nixDarwinModules.default = import ./module.nix self;
+        # use this module before this pr is merged https://github.com/nix-community/home-manager/pull/5304
+        homeManagerModules.prebuiltin = import ./home-manager-module.nix self;
+        # use this module after that pr is merged
+        homeManagerModules.default = import ./module.nix self;
+      };
     };
 }

ToyVo/nh_plus

Containerfile
@@ -1,0 +1,7 @@
+# Make sure I can test builds on linux from macos
+FROM nixos/nix
+RUN mkdir /src
+WORKDIR /src
+ADD . ./
+RUN nix --extra-experimental-features "nix-command flakes" build .#nh --accept-flake-config
+CMD ["./result/bin/nh"]

devshell.nix
@@ -1,33 +1,43 @@
-{ mkShell
-, rust-analyzer-unwrapped
-, rustfmt
-, clippy
-, nvd
-, nix-output-monitor
-, cargo
-, rustc
-, rustPlatform
-,
-}:
-mkShell {
-  strictDeps = true;
+{ inputs, ... }: {
+  perSystem = { pkgs, lib, ... }: {
+    devshells.default = {
+      imports = [
+        "${inputs.devshell}/extra/language/c.nix"
+        # "${devshell}/extra/language/rust.nix"
+      ];
 
-  nativeBuildInputs = [
-    cargo
-    rustc
+      commands = with pkgs; [
+        { package = cargo; category = "rust"; }
+      ];
 
-    rust-analyzer-unwrapped
-    rustfmt
-    clippy
-    nvd
-    nix-output-monitor
-  ];
+      packages = with pkgs; [
+        cargo
+        rustc
+        rust-analyzer-unwrapped
+        rustfmt
+        clippy
+        nvd
+        nix-output-monitor
+      ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.SystemConfiguration ];
 
-  buildInputs = [ ];
+      env = [
+        {
+          name = "NH_NOM";
+          value = "1";
+        }
+        {
+          name = "RUST_LOG";
+          value = "nh=trace";
+        }
+        {
+          name = "RUST_SRC_PATH";
+          value = "${pkgs.rustPlatform.rustLibSrc}";
+        }
+      ];
 
-  env = {
-    NH_NOM = "1";
-    RUST_LOG = "nh=trace";
-    RUST_SRC_PATH = "${rustPlatform.rustLibSrc}";
+      language.c = {
+        libraries = lib.optional pkgs.stdenv.isDarwin pkgs.libiconv;
+      };
+    };
   };
 }

flake.nix
@@ -1,50 +1,57 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self
+    inputs @ { self
     , nixpkgs
-    ,
-    }:
-    let
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-          "aarch64-linux"
-          # experimental
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ]
-          (system: function nixpkgs.legacyPackages.${system});
+    , flake-parts
+    , devshell
+    }: flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-      rev = self.shortRev or self.dirtyShortRev or "dirty";
-    in
-    {
-      overlays.default = final: prev: {
-        nh = self.packages.${final.stdenv.system}.nh;
+      imports = [
+        flake-parts.flakeModules.easyOverlay
+        devshell.flakeModule
+        ./devshell.nix
+      ];
+
+      perSystem = { pkgs, config, ... }: {
+        overlayAttrs = {
+          inherit (config.packages) nh;
+        };
+        packages = rec {
+          nh = pkgs.callPackage ./package.nix {
+            rev = self.shortRev or self.dirtyShortRev or "dirty";
+          };
+          default = nh;
+        };
       };
 
-      packages = forAllSystems (pkgs: rec {
-        nh = pkgs.callPackage ./package.nix {
-          inherit rev;
-        };
-        default = nh;
-      });
-
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.callPackage ./devshell.nix { };
-      });
-
-      nixosModules.default = import ./module.nix self;
-      # use this module before this pr is merged https://github.com/LnL7/nix-darwin/pull/942
-      nixDarwinModules.prebuiltin = import ./darwin-module.nix self;
-      # use this module after that pr is merged
-      nixDarwinModules.default = import ./module.nix self;
-      # use this module before this pr is merged https://github.com/nix-community/home-manager/pull/5304
-      homeManagerModules.prebuiltin = import ./home-manager-module.nix self;
-      # use this module after that pr is merged
-      homeManagerModules.default = import ./module.nix self;
+      flake = {
+        nixosModules.default = import ./module.nix self;
+        # use this module before this pr is merged https://github.com/LnL7/nix-darwin/pull/942
+        nixDarwinModules.prebuiltin = import ./darwin-module.nix self;
+        # use this module after that pr is merged
+        nixDarwinModules.default = import ./module.nix self;
+        # use this module before this pr is merged https://github.com/nix-community/home-manager/pull/5304
+        homeManagerModules.prebuiltin = import ./home-manager-module.nix self;
+        # use this module after that pr is merged
+        homeManagerModules.default = import ./module.nix self;
+      };
     };
 }
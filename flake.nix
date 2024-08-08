f20f0c5b3b4270fed8ad8251a6b5ad73983ffb40
Collin Diekvoss <Collin@Diekvoss.com>
2024-08-08 12:38:48 -0500

    warn -> debug clean profile not found
    Fix url based flakes
    
    crate2nix
    
    fix linux build need to revisit crate2nix
    
    module
    
    darwin-module.nix
    
    crate2nix works
    
    re add modules
    
    cleanup
    
    maybe fixed

--- flake.nix
+++ flake.nix
@@ -1,50 +1,57 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self
+    inputs @ { self
     , nixpkgs
-    ,
-    }:
-    let
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-          "aarch64-linux"
-          # experimental
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ]
-          (system: function nixpkgs.legacyPackages.${system});
+    , flake-parts
+    , devshell
+    }: flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-      rev = self.shortRev or self.dirtyShortRev or "dirty";
-    in
-    {
-      overlays.default = final: prev: {
-        nh = self.packages.${final.stdenv.system}.nh;
+      imports = [
+        flake-parts.flakeModules.easyOverlay
+        devshell.flakeModule
+        ./devshell.nix
+      ];
+
+      perSystem = { pkgs, config, ... }: {
+        overlayAttrs = {
+          inherit (config.packages) nh;
+        };
+        packages = rec {
+          nh = pkgs.callPackage ./package.nix {
+            rev = self.shortRev or self.dirtyShortRev or "dirty";
+          };
+          default = nh;
+        };
       };
 
-      packages = forAllSystems (pkgs: rec {
-        nh = pkgs.callPackage ./package.nix {
-          inherit rev;
-        };
-        default = nh;
-      });
-
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.callPackage ./devshell.nix { };
-      });
-
-      nixosModules.default = import ./module.nix self;
-      # use this module before this pr is merged https://github.com/LnL7/nix-darwin/pull/942
-      nixDarwinModules.prebuiltin = import ./darwin-module.nix self;
-      # use this module after that pr is merged
-      nixDarwinModules.default = import ./module.nix self;
-      # use this module before this pr is merged https://github.com/nix-community/home-manager/pull/5304
-      homeManagerModules.prebuiltin = import ./home-manager-module.nix self;
-      # use this module after that pr is merged
-      homeManagerModules.default = import ./module.nix self;
+      flake = {
+        nixosModules.default = import ./module.nix self;
+        # use this module before this pr is merged https://github.com/LnL7/nix-darwin/pull/942
+        nixDarwinModules.prebuiltin = import ./darwin-module.nix self;
+        # use this module after that pr is merged
+        nixDarwinModules.default = import ./module.nix self;
+        # use this module before this pr is merged https://github.com/nix-community/home-manager/pull/5304
+        homeManagerModules.prebuiltin = import ./home-manager-module.nix self;
+        # use this module after that pr is merged
+        homeManagerModules.default = import ./module.nix self;
+      };
     };
 }
